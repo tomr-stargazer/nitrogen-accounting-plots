0f40791eee969d32acdae68aa47633aaa7b3c2f9
Tom Rice <t.rice90@gmail.com>
2016-12-12 13:28:35 -0500

    wrote fns for making figs 10, 11 goldsmith

--- reproduce_goldsmith_plume_plots.py
+++ reproduce_goldsmith_plume_plots.py
@@ -64,3 +64,117 @@ def make_goldsmith_figure_9(save=True, column=1e16, abundance=1.7e-7):
 
     return fig
 
+
+def make_goldsmith_figure_10(save=True):
+    """ Generates my version of Fig. 10 from Goldsmith+'97 """
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    density_array = np.logspace(1, 7, 100)
+    temperature_list = [10, 30, 50]
+
+    f_c_array_10K = np.zeros_like(density_array)
+    f_c_array_30K = np.zeros_like(density_array)
+    f_c_array_50K = np.zeros_like(density_array)
+
+    f_c_array_list = [f_c_array_10K, f_c_array_30K, f_c_array_50K]
+
+    for j, temp in enumerate(temperature_list):
+
+        # Here we have to specify 2 of 3: abundance, temperature, density.
+        R = pyradex.Radex(species='c18o', abundance=1.7e-7, column=1e14, temperature=temp)
+
+        for i, rho in enumerate(density_array):
+
+            new_T = R(collider_densities={'H2':rho})
+
+            # use the correction_factor code here!
+            f_c = correction_factor_given_radex_table(new_T, [0, 1])
+
+            f_c_array_list[j][i] = f_c
+            
+    ax.plot(np.log10(density_array), f_c_array_10K, '-', lw=1.5)
+    ax.plot(np.log10(density_array), f_c_array_30K, ':', lw=2)
+    ax.plot(np.log10(density_array), f_c_array_50K, '--', lw=1.5)
+
+    ax.set_ylim(1, 3)
+    ax.set_xlim(2, 7.5)
+
+    fontdict = {'family':'serif'}
+
+    ax.set_ylabel("CORRECTION FACTOR FOR COLUMN DENSITY", fontdict=fontdict)
+    ax.set_xlabel("LOG (MOLECULAR HYDROGEN DENSITY/CM$^{-3}$)", fontdict=fontdict)
+
+    ax.text(4.5, 2.8, "C$^{18}$O", fontsize=18, family='serif')
+    ax.text(6, 2.8, r"$T_k = 50\ \rm{K}$", fontsize=16, family='serif', color='r')
+    ax.text(6, 2.2, r"$T_k = 30\ \rm{K}$", fontsize=16, family='serif', color='g')
+    ax.text(6, 1.55, r"$T_k = 10\ \rm{K}$", fontsize=16, family='serif', color='b')
+    ax.text(2.5, 2.5, "$J=1-0$ & $J=2-1$ OBSERVED", fontsize=14, family='serif')
+
+    ax.minorticks_on()
+
+    if save:
+        fig.savefig("goldsmith97_fig_10_reproduction.pdf")
+
+    plt.show()
+
+    return fig
+
+
+def make_goldsmith_figure_11(save=True):
+    """ Generates my version of Fig. 11 from Goldsmith+'97 """
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    density_array = np.logspace(1, 7, 100)
+    temperature_list = [10, 30, 50]
+
+    f_c_array_10K = np.zeros_like(density_array)
+    f_c_array_30K = np.zeros_like(density_array)
+    f_c_array_50K = np.zeros_like(density_array)
+
+    f_c_array_list = [f_c_array_10K, f_c_array_30K, f_c_array_50K]
+
+    for j, temp in enumerate(temperature_list):
+
+        # Here we have to specify 2 of 3: abundance, temperature, density.
+        R = pyradex.Radex(species='c18o', abundance=1.7e-7, column=1e14, temperature=temp)
+
+        for i, rho in enumerate(density_array):
+
+            new_T = R(collider_densities={'H2':rho})
+
+            # use the correction_factor code here!
+            f_c = correction_factor_given_radex_table(new_T, [0, 1, 2])
+
+            f_c_array_list[j][i] = f_c
+
+            
+    ax.plot(np.log10(density_array), f_c_array_10K, '-', lw=1.5)
+    ax.plot(np.log10(density_array), f_c_array_30K, ':', lw=2)
+    ax.plot(np.log10(density_array), f_c_array_50K, '--', lw=1.5)
+
+    ax.set_ylim(1, 3)
+    ax.set_xlim(2, 7.5)
+
+    fontdict = {'family':'serif'}
+
+    ax.set_ylabel("CORRECTION FACTOR FOR COLUMN DENSITY", fontdict=fontdict)
+    ax.set_xlabel("LOG (MOLECULAR HYDROGEN DENSITY/CM$^{-3}$)", fontdict=fontdict)
+
+    ax.text(4.5, 2.8, "C$^{18}$O", fontsize=18, family='serif')
+    ax.text(6, 2, r"$T_k = 50\ \rm{K}$", fontsize=16, family='serif', color='r')
+    ax.text(6, 1.55, r"$T_k = 30\ \rm{K}$", fontsize=16, family='serif', color='g')
+    ax.text(6, 1.2, r"$T_k = 10\ \rm{K}$", fontsize=16, family='serif', color='b')
+    ax.text(2.5, 2.5, "$J=1-0$, $J=2-1$, & $J=3-2$ OBSERVED", fontsize=14, family='serif')
+
+    ax.minorticks_on()
+
+    if save:
+        fig.savefig("goldsmith97_fig_11_reproduction.pdf")
+
+    plt.show()
+
+    return fig
