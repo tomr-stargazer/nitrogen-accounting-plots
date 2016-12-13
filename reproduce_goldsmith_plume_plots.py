651332a1014260577ac4b3ff635775cdf4dfd32a
Tom Rice <t.rice90@gmail.com>
2016-12-13 18:50:36 -0500

    I wrote the Plume code

--- reproduce_goldsmith_plume_plots.py
+++ reproduce_goldsmith_plume_plots.py
@@ -193,3 +193,83 @@ def make_goldsmith_figure_11(save=True):
     plt.show()
 
     return fig
+
+
+def make_plume_figure_3(save=True, print_timing=False, abundance=5e-7, n_points=20):
+    """ Generates my version of Fig. 3 from Plume+'12 """
+
+    # Plume '12 claims to use an abundance of 2e-7 (i.e. one C18O for every 500 x 10^4 H2 atoms)
+    # but I find that 5e-7 matches the appearance much more closely.
+
+    start = time.time()
+
+    # Figure 3's caption says that Ju=10 is included, but the text omits it. 
+    # I have had better luck reproducing the plot when I omit Ju=10,
+    # so I infer that the caption is incorrect.
+    J_upper_list = [2,3,5,7,8,9,11,15]
+    J_lower_list = [x-1 for x in J_upper_list]
+
+    # Here we have to specify 2 of 3: abundance, temperature, density.
+    R = pyradex.Radex(species='c18o', abundance=abundance, column=5e16, temperature=50, escapeProbGeom='lvg')
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    density_list = [1e5, 1e6, 1e7]
+    temperature_array = np.linspace(10, 400, n_points)
+
+    f_c_array_n5 = np.zeros_like(temperature_array)
+    f_c_array_n6 = np.zeros_like(temperature_array)
+    f_c_array_n7 = np.zeros_like(temperature_array)
+
+    f_c_array_list = [f_c_array_n5, f_c_array_n6, f_c_array_n7]
+
+    for j, density in enumerate(density_list):
+
+        R.density = density
+
+        for i, temp in enumerate(temperature_array):
+
+            new_T = R(temperature=temp)
+
+            # use the correction_factor code here!
+            f_c = correction_factor_given_radex_table(new_T, J_lower_list, included_levels=18)
+            # They only use the lowest 18 rotational levels, 
+            # even though the plot's appearance changes when 40 are included.
+
+            f_c_array_list[j][i] = f_c
+            
+    ax.plot(temperature_array, f_c_array_n7, 'b-', lw=1.5, label=r'$n = 10^7\ \rm{cm}^{-3}$')
+    ax.plot(temperature_array, f_c_array_n6, 'g--', lw=1.5, label=r'$n = 10^6\ \rm{cm}^{-3}$')
+    ax.plot(temperature_array, f_c_array_n5, 'r:', lw=2, label=r'$n = 10^5\ \rm{cm}^{-3}$')
+
+    ax.legend(frameon=False, loc='upper center', fontsize=18)
+
+    ax.set_xlim(0, 400)
+    ax.set_ylim(1.6, 2)
+    ax.set_xticks([0,100,200,300,400])
+    ax.set_yticks([1.6,1.7,1.8,1.9,2])
+
+    fontdict = {'family':'serif', 'fontsize':16}
+
+    ax.set_ylabel("Correction Factor", fontdict=fontdict)
+    ax.set_xlabel("Temp (K)", fontdict=fontdict)
+
+    # ax.text(4.5, 2.8, "C$^{18}$O", fontsize=18, family='serif')
+    # ax.text(6, 2.8, r"$T_k = 50\ \rm{K}$", fontsize=16, family='serif', color='r')
+    # ax.text(6, 2.2, r"$T_k = 30\ \rm{K}$", fontsize=16, family='serif', color='g')
+    # ax.text(6, 1.55, r"$T_k = 10\ \rm{K}$", fontsize=16, family='serif', color='b')
+    # ax.text(2.5, 2.5, "$J=1-0$ & $J=2-1$ OBSERVED", fontsize=14, family='serif')
+
+    ax.minorticks_on()
+
+    if save:
+        fig.savefig("plume12_fig_3_reproduction.pdf")
+
+    plt.show()
+
+    end = time.time()
+    if print_timing:
+        print end-start
+
+    return fig    
