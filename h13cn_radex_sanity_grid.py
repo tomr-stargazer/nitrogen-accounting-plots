8d68210c875a037a571480e89321f037b164ccf1
Tom Rice <t.rice90@gmail.com>
2017-01-19 12:16:50 -0500

    revising from an abundance-oriented to columndensity-oriented approach

--- h13cn_radex_sanity_grid.py
+++ h13cn_radex_sanity_grid.py
@@ -35,85 +35,85 @@ sanity_grid = np.zeros((n_temperatures, m_logdensities))
 iters_grid = np.zeros((n_temperatures, m_logdensities))
 
 # Initialize radex object
-initial_column=1e16 
-initial_abundance=7e-8
-R = pyradex.Radex(species='h13cn@xpol', abundance=initial_abundance, temperature=20, 
+initial_column=1e08
+initial_density=1e4
+R = pyradex.Radex(species='h13cn@xpol', temperature=20, density=initial_density,
                   column=initial_column)
 R.maxiter = 1000
 
-abundance_list = [2e-9, 2e-8, 7e-8]
+# abundance_list = [2e-9, 2e-8, 7e-8]
 
-for abundance in abundance_list:
+# for abundance in abundance_list:
 
-    R.abundance = abundance
+    # R.abundance = abundance
 
-    for i, temp in enumerate(temperatures):
+for i, temp in enumerate(temperatures):
 
-        R.temperature = temp
+    R.temperature = temp
 
-        for j, logdens in enumerate(logdensities):
+    for j, logdens in enumerate(logdensities):
 
-            R.density = 10**logdens
+        R.density = 10**logdens
 
-            # calculate the T thing
+        # calculate the T thing
 
-            if j == 0:
-                n_iters = R.run_radex(reuse_last=False, reload_molfile=True, silent=False)
-            else:
-                n_iters = R.run_radex()
+        if j == 0:
+            n_iters = R.run_radex(reuse_last=False, reload_molfile=True, silent=False)
+        else:
+            n_iters = R.run_radex()
 
-            T = R.get_table()
+        T = R.get_table()
 
-            print "Tkin = {0}, logdens = {1}, column = {2}".format(R.temperature, 
-                                                                   np.log10(R.density['H2'].value), 
-                                                                   R.column)
-            print "Tex of 1-0 line: {0}".format(T['Tex'][0])
-            print "n_iters={1}. Sum of lower level pops: {0}".format(np.sum(T['lowerlevelpop']), 
-                                                                     n_iters)
-            print ""
+        print "Tkin = {0}, logdens = {1}, column = {2}".format(R.temperature, 
+                                                               np.log10(R.density['H2'].value), 
+                                                               R.column)
+        print "Tex of 1-0 line: {0}".format(T['Tex'][0])
+        print "n_iters={1}. Sum of lower level pops: {0}".format(np.sum(T['lowerlevelpop']), 
+                                                                 n_iters)
+        print ""
 
-            negative_tex_count = len(T[T['Tex'] < 0])
+        negative_tex_count = len(T[T['Tex'] < 0])
 
-            sanity_grid[i,j] = negative_tex_count
-            iters_grid[i,j] = n_iters
-
-
-            # if (min(T['Tex']) < 0) and (np.sum(T['lowerlevelpop']) < 0.99):
-            #     # this will FAIL on last iteration if I mixed up my axis order
-            #     sanity_grid[i,j] = -2
-            # elif min(T['Tex']) < 0:
-            #     sanity_grid[i,j] = -1
-            # elif np.sum(T['lowerlevelpop']) < 0.99:
-            #     sanity_grid[i,j] = 0
-            # else:
-            #     sanity_grid[i,j] = 1
+        sanity_grid[i,j] = negative_tex_count
+        iters_grid[i,j] = n_iters
 
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.imshow(sanity_grid, origin='lower', extent=(min_logdens, max_logdens, min_temp, max_temp),
-              interpolation='nearest', aspect=(max_logdens-min_logdens)/(max_temp-min_temp), 
-              cmap='Reds')
+        # if (min(T['Tex']) < 0) and (np.sum(T['lowerlevelpop']) < 0.99):
+        #     # this will FAIL on last iteration if I mixed up my axis order
+        #     sanity_grid[i,j] = -2
+        # elif min(T['Tex']) < 0:
+        #     sanity_grid[i,j] = -1
+        # elif np.sum(T['lowerlevelpop']) < 0.99:
+        #     sanity_grid[i,j] = 0
+        # else:
+        #     sanity_grid[i,j] = 1
 
-    fontdict = {'family':'serif', 'fontsize':16}
 
-    ax.set_xlabel("log (H$_2$ density/cm$^{-3}$)", fontdict=fontdict)
-    ax.set_ylabel("Temp (K)", fontdict=fontdict)
-    ax.set_title("H$^{13}$CN Column density: %.1e, abundance=%.1e" % (R.column.value, R.abundance))
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.imshow(sanity_grid, origin='lower', extent=(min_logdens, max_logdens, min_temp, max_temp),
+          interpolation='nearest', aspect=(max_logdens-min_logdens)/(max_temp-min_temp), 
+          cmap='Reds')
 
-    plt.savefig("X{0:1e}.png".format(abundance))
+fontdict = {'family':'serif', 'fontsize':16}
 
-    fig2 = plt.figure()
-    ax2 = fig2.add_subplot(111)
-    ax2.imshow(iters_grid, origin='lower', extent=(min_logdens, max_logdens, min_temp, max_temp),
-              interpolation='nearest', aspect=(max_logdens-min_logdens)/(max_temp-min_temp), 
-              cmap='Blues')
+ax.set_xlabel("log (H$_2$ density/cm$^{-3}$)", fontdict=fontdict)
+ax.set_ylabel("Temp (K)", fontdict=fontdict)
+ax.set_title("H$^{13}$CN Column density: %.1e, abundance=%.1e" % (R.column.value, R.abundance))
 
-    fontdict = {'family':'serif', 'fontsize':16}
+plt.savefig("N{0:1e}.png".format(R.column.value))
 
-    ax2.set_xlabel("log (H$_2$ density/cm$^{-3}$)", fontdict=fontdict)
-    ax2.set_ylabel("Temp (K)", fontdict=fontdict)
-    ax2.set_title("H$^{13}$CN Column density: %.1e, abundance=%.1e" % (R.column.value, R.abundance))
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(111)
+ax2.imshow(iters_grid, origin='lower', extent=(min_logdens, max_logdens, min_temp, max_temp),
+          interpolation='nearest', aspect=(max_logdens-min_logdens)/(max_temp-min_temp), 
+          cmap='Blues')
 
-    plt.show()
+fontdict = {'family':'serif', 'fontsize':16}
+
+ax2.set_xlabel("log (H$_2$ density/cm$^{-3}$)", fontdict=fontdict)
+ax2.set_ylabel("Temp (K)", fontdict=fontdict)
+ax2.set_title("H$^{13}$CN Column density: %.1e, abundance=%.1e" % (R.column.value, R.abundance))
+
+plt.show()
 
