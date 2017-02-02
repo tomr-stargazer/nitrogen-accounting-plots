429eda9800797dd84940125ac207344acd970b8a
Tom Rice <t.rice90@gmail.com>
2017-02-02 10:33:16 -0500

    aborted attempt to include c18o myradex stuff

--- reproduce_goldsmith_plume_plots.py
+++ reproduce_goldsmith_plume_plots.py
@@ -13,6 +13,7 @@ import pyradex
 
 # This import will have to change if I refactor correction_factor much.
 from correction_factor.correction_factor_2 import correction_factor_given_radex_table
+from c18o_myradex_interface import myradex_c18o
 
 # Default values come from Goldsmith/Bergin/Lis '97, esp. the c18o abundance
 def make_goldsmith_figure_9(save=True, column=1e16, abundance=1.7e-7, print_timing=False):
@@ -71,6 +72,60 @@ def make_goldsmith_figure_9(save=True, column=1e16, abundance=1.7e-7, print_timi
 
     return fig
 
+# Default values come from Goldsmith/Bergin/Lis '97, esp. the c18o abundance
+# def make_goldsmith_figure_9_myradex(save=True, column=1e16, abundance=1.7e-7, print_timing=False):
+#     """ Generates my version of Fig. 9 from Goldsmith+'97, now with myradex """
+
+#     start = time.time()
+
+#     fig = plt.figure()
+#     ax = fig.add_subplot(111)
+
+#     density_array = np.logspace(1, 6.5, 100)
+
+#     Tex_array_1_0 = np.zeros_like(density_array)
+#     Tex_array_2_1 = np.zeros_like(density_array)
+#     Tex_array_3_2 = np.zeros_like(density_array)
+
+#     for i, rho in enumerate(density_array):
+
+#         od = c18o_myradex_interface(kinetic_temperature=20, column_density=column, collider_density=rho)
+
+#         new_T = od['table']
+
+#         Tex_array_1_0[i] = new_T[0]['Tex']
+#         Tex_array_2_1[i] = new_T[1]['Tex']
+#         Tex_array_3_2[i] = new_T[2]['Tex']
+
+#     ax.plot(np.log10(density_array), Tex_array_1_0, '-', lw=1.5)
+#     ax.plot(np.log10(density_array), Tex_array_2_1, ':', lw=2)
+#     ax.plot(np.log10(density_array), Tex_array_3_2, '--', lw=1.5)
+
+#     ax.set_ylim(0,25)
+#     ax.set_xlim(1,6.5)
+
+#     fontdict = {'family':'serif'}
+
+#     ax.set_ylabel("EXCITATION TEMPERATURE/K", fontdict=fontdict)
+#     ax.set_xlabel("LOG (MOLECULAR HYDROGEN DENSITY/CM$^{-3}$)", fontdict=fontdict)
+
+#     ax.text(1.5, 20, "C$^{18}$O", fontsize=18, family='serif')
+#     ax.text(2.35, 15, "$J = 1 - 0$", fontsize=16, family='serif', color='b')
+#     ax.text(3.6, 18, "$J = 2 - 1$", fontsize=16, family='serif', color='g')
+#     ax.text(4.1, 10, "$J = 3 - 2$", fontsize=16, family='serif', color='r')
+
+#     ax.minorticks_on()
+
+#     if save:
+#         fig.savefig("goldsmith97_fig_9_reproduction_myradex.pdf")
+
+#     plt.show()
+
+#     end = time.time()
+#     if print_timing:
+#         print end-start
+
+#     return fig
 
 def make_goldsmith_figure_10(save=True, print_timing=False):
     """ Generates my version of Fig. 10 from Goldsmith+'97 """
