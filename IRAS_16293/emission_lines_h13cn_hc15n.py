28e062871fff4cfe658ecd8c786ba3454c4b187d
Tom Rice <t.rice90@gmail.com>
2017-02-03 14:26:51 -0500

    starting this out right.

---
+++ IRAS_16293/emission_lines_h13cn_hc15n.py
@@ -0,0 +1,23 @@
+"""
+this loads the Herschel emission line data from elsewhere and prepares it for plotting. Includes a function which makes a multi-panel plot.
+
+"""
+
+from __future__ import division
+
+# Let's LOAD the data from somewhere... then make it plottable!
+
+data_path = None
+
+def load_HIFI_spectral_data():
+    """ Loads the HIFI spectral data. """
+
+    # What am I doing specifically?
+
+    pass
+
+
+def plot_h13cn_hc15n_lines_with_fits():
+    """ Plot the emission lines and their Gaussian fits. """
+
+    pass
