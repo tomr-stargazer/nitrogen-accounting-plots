0d4ff6755e59ee199966e2cc33d6c83fd6f24d19
Tom Rice <t.rice90@gmail.com>
2017-06-16 02:10:08 -0400

    ok so added a new big important fn

--- accounting/new_accounting.py
+++ accounting/new_accounting.py
@@ -13,6 +13,7 @@ So anyway, this code is for doing it right.
 
 """
 
+import pdb
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -84,12 +85,58 @@ def plot_organic_nitrogen_fraction(table):
     ax.set_xticklabels(organic_N_table['Molecule'], rotation=90)
 
     ax.set_ylabel("Organic N fraction")
+    ax.set_xticks([])
 
     ax.semilogy()
     ax.set_ylim(0.005, 1.5)
     plt.yticks(
         (1e-2, 3e-2, 1e-1, 0.3, 0.6, 1), 
         (r"1%", r"3%", r"10%", r"30%", r"60%",r"100%"), fontsize=8)
+
+    return fig
+
+
+def plot_organic_nitrogen_fraction_w_colors_and_errors(table):
+    """ starting basic then adding bells-and-whistles like sorting. """
+
+    fig = plt.figure(figsize=(2.5, 2.5), dpi=400)
+    ax = fig.add_subplot(111)
+
+    organic_N_table = table[['N' in x['Molecule'] and 'C' in x['Molecule'] and ~np.isnan(x['Hot Core']) for x in table]]
+    organic_N_table.sort("Hot Core")
+    organic_N_table.reverse()
+
+    naive_total_organic_N = np.sum(organic_N_table['Hot Core'])
+
+    colors = ['tab:red', 'tab:purple', 'tab:green', 'tab:olive', 'tab:cyan', 'tab:pink', 'tab:brown', 'tab:orange']
+    for i, color in enumerate(colors):
+
+        total_organic_N_others = naive_total_organic_N - organic_N_table['Hot Core'][i]
+        fraction = organic_N_table['Hot Core'][i]/naive_total_organic_N
+        abundance_high = organic_N_table['Hot Core'][i]*1.4
+        fraction_high = abundance_high/(total_organic_N_others+abundance_high)
+        abundance_low = organic_N_table['Hot Core'][i]*0.6
+        fraction_low = abundance_low/(total_organic_N_others+abundance_low)
+
+        ax.errorbar(i, fraction, yerr=np.array([fraction-fraction_low, fraction_high-fraction]).reshape(2,1), 
+                    fmt='o', ms=3.5, color=color, ecolor=color, capsize=3, elinewidth=1.5, capthick=1.5)
+
+
+    # ax.plot(organic_N_table['Hot Core']/total_organic_N, 'ro')
+    ax.set_xticks(np.arange(len(organic_N_table)))
+    ax.set_xticklabels(organic_N_table['Molecule'], rotation=90)
+
+    ax.set_ylabel("Organic N fraction")
+    ax.set_xticks([])
+
+    ax.semilogy()
+    ax.set_ylim(0.004, 1.5)
+    plt.gca().yaxis.grid()
+    plt.yticks(
+        (0.004, 1e-2, 3e-2, 1e-1, 0.3, 0.6, 1), 
+        (r"0.4%", r"1%", r"3%", r"10%", r"30%", r"60%",r"100%"), fontsize=8)
+
+    plt.savefig("new_nitrogen_fraction_plot_for_poster.png", bbox_inches='tight')
 
     return fig
 
