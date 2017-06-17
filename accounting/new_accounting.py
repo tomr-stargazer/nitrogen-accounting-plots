c29d5eef586559c182caab71ca5d61873e72b990
Tom Rice <t.rice90@gmail.com>
2017-06-17 14:13:19 -0400

    finished the new accounting for GRC

--- accounting/new_accounting.py
+++ accounting/new_accounting.py
@@ -42,7 +42,19 @@ def load_table_7_Zernickel():
     table7_zernickel = astropy.table.Table.read("zernickel_table7_just_the_table.html", header_start=0, data_start=4)
 
     return table7_zernickel
-    
+
+
+def transform_table_7_Zernickel(table):
+
+    # so basically, we wanna ditch the other columns and get out abundances (relative to N_H2 like a normal person).
+
+    Xz_values = np.array([float(x.replace("(","e").replace("–","-").rstrip(")")) for x in table['NGC 6334I']])
+    Xs = astropy.table.Column(2*Xz_values, name="Abundance (X/H2)")
+
+    new_table = astropy.table.Table([table['Molecule'], Xs])
+
+    return new_table
+
 
 def plot_molecular_abundances(table):
     """ starting basic then adding bells-and-whistles like sorting. """
@@ -103,33 +115,49 @@ def plot_organic_nitrogen_fraction(table):
     return fig
 
 
-def plot_organic_nitrogen_fraction_w_colors_and_errors(table):
+orion_KL_colors = ['tab:red', 'tab:purple', 'tab:green', 'tab:olive', 'tab:cyan', 'tab:pink', 'tab:brown', 'tab:orange']
+orion_KL_savename = "new_nitrogen_fraction_plot_for_poster.png"
+orion_KL_ylims = (0.003, 1.5)
+orion_KL_yticks = ((0.003, 1e-2, 3e-2, 1e-1, 0.3, 0.6, 1), 
+                   (r"0.3%", r"1%", r"3%", r"10%", r"30%", r"60%",r"100%"))
+ngc6334_colors = ['tab:red', 'tab:purple', 'tab:olive', 'tab:green', 'tab:pink', 'tab:gray']
+ngc6334_savename = "zern_new_plot.png"
+ngc6334_ylims = (0.003, 1.5)
+
+
+def plot_organic_nitrogen_fraction_w_colors_and_errors(
+    table, abundance_colname='Hot Core', ylims=orion_KL_ylims, yticks=orion_KL_yticks, colors=orion_KL_colors, 
+    savename=orion_KL_savename):
     """ starting basic then adding bells-and-whistles like sorting. """
 
     fig = plt.figure(figsize=(2.5, 2.5), dpi=400)
     ax = fig.add_subplot(111)
 
-    organic_N_table = table[['N' in x['Molecule'] and 'C' in x['Molecule'] and ~np.isnan(x['Hot Core']) for x in table]]
-    organic_N_table.sort("Hot Core")
+    organic_N_table = table[['N' in x['Molecule'] and 'C' in x['Molecule'] and ~np.isnan(x[abundance_colname]) for x in table]]
+    organic_N_table.sort(abundance_colname)
     organic_N_table.reverse()
 
-    naive_total_organic_N = np.sum(organic_N_table['Hot Core'])
+    naive_total_organic_N = np.sum(organic_N_table[abundance_colname])
 
-    colors = ['tab:red', 'tab:purple', 'tab:green', 'tab:olive', 'tab:cyan', 'tab:pink', 'tab:brown', 'tab:orange']
+    # colors = ['tab:red', 'tab:purple', 'tab:green', 'tab:olive', 'tab:cyan', 'tab:pink', 'tab:brown', 'tab:orange']
     for i, color in enumerate(colors):
 
-        total_organic_N_others = naive_total_organic_N - organic_N_table['Hot Core'][i]
-        fraction = organic_N_table['Hot Core'][i]/naive_total_organic_N
-        abundance_high = organic_N_table['Hot Core'][i]*1.4
+        total_organic_N_others = naive_total_organic_N - organic_N_table[abundance_colname][i]
+        fraction = organic_N_table[abundance_colname][i]/naive_total_organic_N
+        abundance_high = organic_N_table[abundance_colname][i]*1.4
         fraction_high = abundance_high/(total_organic_N_others+abundance_high)
-        abundance_low = organic_N_table['Hot Core'][i]*0.6
+        abundance_low = organic_N_table[abundance_colname][i]*0.6
         fraction_low = abundance_low/(total_organic_N_others+abundance_low)
+
+        if organic_N_table['Molecule'][i] == 'HCN':
+            print(fraction, fraction_high, fraction_low)
+            print(organic_N_table[abundance_colname][i], abundance_high, abundance_low)
 
         ax.errorbar(i, fraction, yerr=np.array([fraction-fraction_low, fraction_high-fraction]).reshape(2,1), 
                     fmt='o', ms=3.5, color=color, ecolor=color, capsize=3, elinewidth=1.5, capthick=1.5)
 
 
-    # ax.plot(organic_N_table['Hot Core']/total_organic_N, 'ro')
+    # ax.plot(organic_N_table[abundance_colname]/total_organic_N, 'ro')
     ax.set_xticks(np.arange(len(organic_N_table)))
     ax.set_xticklabels(organic_N_table['Molecule'], rotation=90)
 
@@ -137,15 +165,22 @@ def plot_organic_nitrogen_fraction_w_colors_and_errors(table):
     ax.set_xticks([])
 
     ax.semilogy()
-    ax.set_ylim(0.004, 1.5)
+    ax.set_ylim(ylims)
     plt.gca().yaxis.grid()
-    plt.yticks(
-        (0.004, 1e-2, 3e-2, 1e-1, 0.3, 0.6, 1), 
-        (r"0.4%", r"1%", r"3%", r"10%", r"30%", r"60%",r"100%"), fontsize=8)
+    plt.yticks(*yticks, fontsize=8)
 
-    plt.savefig("new_nitrogen_fraction_plot_for_poster.png", bbox_inches='tight')
+    if savename is not None:
+        plt.savefig(savename, bbox_inches='tight')
 
     return fig
+
+
+def plot_organic_nitrogen_fraction_w_colors_and_errors_zernickel(savename=ngc6334_savename):
+
+    table = transform_table_7_Zernickel(load_table_7_Zernickel())
+
+    return plot_organic_nitrogen_fraction_w_colors_and_errors(
+         table, abundance_colname='Abundance (X/H2)', colors=ngc6334_colors, ylims=ngc6334_ylims, savename=savename)
 
 
 def get_N_molecules(table):
