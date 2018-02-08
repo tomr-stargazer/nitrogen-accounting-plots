2733bb4abd38a6e88ef5f1397adc66fd8f498004
Tom Rice <t.rice90@gmail.com>
2018-02-08 16:54:56 -0500

    initial commit for N/H2O plot

---
+++ cometary_comparisons/N_H2O_plot.py
@@ -0,0 +1,13 @@
+"""
+Let's make a basic plot showing N/H2O values from protostars, comets, and ISM dust.
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
